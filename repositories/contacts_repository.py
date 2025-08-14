c3614074d3cd99c27d4c3228df696d9b3b4f8f3c
Jose <jose.vitor.araujo569@hotmail.com>
2025-08-14 20:58:29 -0300

    chore: fix on limit get_contacts

--- repositories/contacts_repository.py
+++ repositories/contacts_repository.py
@@ -1,6 +1,7 @@
 from .supabase_client import supabase
 
 
-def get_contacts(limit=3):
-    response = supabase.table("contacts").select("*").limit(limit).execute()
+def get_contacts(limit=1):
+    response = (supabase.table("contacts").select(
+        "*").order("id", desc=False).range(0, limit - 1).execute())
     return response.data or []
